25b3b5dfd6735984ff49faca619ff0beb9a00d8d
sepi_sim <118772630+Sepi1010011@users.noreply.github.com>
2024-04-15 21:13:15 +0330

    Cleaning and changing positional encoding

--- decoder_only_transformer.py
+++ decoder_only_transformer.py
@@ -1,14 +1,9 @@
-from django.test import tag
-from matplotlib import axis
-from matplotlib.pyplot import sca
 import numpy as np
-import random
 import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F 
-from torch.utils.data import Dataset, DataLoader
-from torch.cuda.amp import autocast, GradScaler
+
 
 # INPUT EMBEDDING 
 # POSITIONAL EMBEDDING 
@@ -32,80 +27,13 @@ ffn_hidden = 2048
 START_TOKEN = '<start>'
 END_TOKEN = '<end>'
 PADDING_TOKEN = '<pad>'
-UNK_TOKEN = '<unk>'
 TOTAL_SENTENCES = 100000
 max_sequence_length = 2048
-english_vocabulary = [START_TOKEN, UNK_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',':', '<', '=', '>', '?', '@','[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', PADDING_TOKEN, END_TOKEN]
+english_vocabulary = [START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',':', '<', '=', '>', '?', '@','[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', PADDING_TOKEN, END_TOKEN]
 en_vocab_size = len(english_vocabulary)
 language_to_index = {v:k for k,v in enumerate(english_vocabulary)}
 index_to_language = {k:v for k,v in enumerate(english_vocabulary)}
 
-file_input = open("input.txt", 'r')
-dataset_input = file_input.readlines()
-
-file_target = open("target.txt", 'r')
-dataset_target = file_target.readlines()
-
-dataset_input = [sentence.replace('\n', '') for sentence in dataset_input]
-dataset_target = [sentence.replace('\n', '') for sentence in dataset_target]
-
-
-
-def choose_random_line(dataset1, dataset2):
-    input, target = [], []
-    indices = list(range(len(dataset1)))
-    random.shuffle(indices)
-    selected_inidcies = indices[:TOTAL_SENTENCES]
-    
-    for index in selected_inidcies:
-        input.append(dataset1[index])
-        target.append(dataset2[index])
-    
-    return input, target
-    
-
-randomly_input, randomly_target = choose_random_line(dataset_input, dataset_target)
-
-# tokenizer = get_tokenizer('basic_english')
-
-# # Vocabulary
-# def yield_tokens(data):
-#     for text in data:
-#         yield tokenizer(text)
-
-# vocab = build_vocab_from_iterator(yield_tokens(dataset_input + dataset_target), specials=[START_TOKEN, END_TOKEN, PADDING_TOKEN, UNK_TOKEN])
-# vocab.set_default_index(vocab[UNK_TOKEN])
-
-
-
-# def collate_batch(batch):
-#     input_batch, target_batch = [], []
-#     for input_text, target_text in batch:
-#         input_indices = [vocab[token] for token in tokenizer(input_text)]
-#         target_indices = [vocab[token] for token in tokenizer(target_text)]
-#         input_batch.append(input_indices)
-#         target_batch.append(target_indices)
-#     return input_batch, target_batch
-
-
-
-
-class TextDataset(Dataset):
-
-    def __init__(self, input, target):
-        self.input = input
-        self.target = target
-
-    def __len__(self):
-        return len(self.input)
-
-    def __getitem__(self, idx):
-        return self.input[idx], self.target[idx]
-
-
-dataset = TextDataset(dataset_input, dataset_target)
-train_loader = DataLoader(dataset, batch_size)
-# train_loader = DataLoader(dataset, batch_size, collate_fn=collate_batch)
 
 def get_device():
     return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -139,26 +67,20 @@ def create_decoder_masks(sentence_batch):
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_sequence_length):
         super().__init__()
-    #     self.max_sequence_length = max_sequence_length
-    #     self.d_model = d_model
-
-    # def forward(self):
-    #     even_i = torch.arange(0, self.d_model, 2).float()
-    #     denominator = torch.pow(10000, even_i/self.d_model)
-    #     position = (torch.arange(self.max_sequence_length).reshape(self.max_sequence_length, 1))
-    #     even_PE = torch.sin(position / denominator)
-    #     odd_PE = torch.cos(position / denominator)
-    #     stacked = torch.stack([even_PE, odd_PE], dim=2)
-    #     PE = torch.flatten(stacked, start_dim=1, end_dim=2)
-    #     return PE
+        self.max_sequence_length = max_sequence_length
+        self.d_model = d_model
         self.position_embedding = nn.Embedding(max_sequence_length, d_model)
-    
-    def forward(self, x):
-        seq_length = x.size(1)
-        positions = torch.arange(0, seq_length, device=x.device).unsqueeze(0).expand(x.size(0), -1)
-        position_embeddings = self.position_embedding(positions)
-        return x + position_embeddings
 
+    def forward(self):
+        even_i = torch.arange(0, self.d_model, 2).float()
+        denominator = torch.pow(10000, even_i/self.d_model)
+        position = (torch.arange(self.max_sequence_length).reshape(self.max_sequence_length, 1))
+        even_PE = torch.sin(position / denominator)
+        odd_PE = torch.cos(position / denominator)
+        stacked = torch.stack([even_PE, odd_PE], dim=2)
+        PE = torch.flatten(stacked, start_dim=1, end_dim=2)
+        return PE
+    
 
 class SentenceEmbedding(nn.Module):
     "For a given sentence, create an embedding"
@@ -195,7 +117,7 @@ class SentenceEmbedding(nn.Module):
     def forward(self, x, start_token, end_token): 
         x = self.batch_tokenize(x, start_token, end_token)
         x = self.embedding(x)
-        pos = self.position_encoder(x).to(get_device())
+        pos = self.position_encoder().to(get_device())
         x = self.dropout(x + pos)
         return x
 
@@ -244,12 +166,12 @@ class PositionwiseFeedForward(nn.Module):
         super(PositionwiseFeedForward, self).__init__()
         self.linear1 = nn.Linear(d_model, hidden)
         self.linear2 = nn.Linear(hidden, d_model)
-        self.relu = nn.ReLU()
+        self.gelu = nn.GELU()
         self.dropout = nn.Dropout(p=drop_prob)
 
     def forward(self, x):
         x = self.linear1(x)
-        x = self.relu(x)
+        x = self.gelu(x)
         x = self.dropout(x)
         x = self.linear2(x)
         return x
@@ -351,63 +273,3 @@ class GPTLanguageModel(nn.Module):
         out = self.softmax(out)
         return out
     
-
-transformer = GPTLanguageModel(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, en_vocab_size, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
-
-# Define loss function and optimizer
-for params in transformer.parameters():
-    if params.dim() > 1:
-        nn.init.xavier_uniform_(params)
-        
-criterion = nn.CrossEntropyLoss(reduction='none')
-optimizer = torch.optim.Adam(transformer.parameters(), lr=learning_rate)
-device = get_device()
-
-transformer.train()
-transformer.to(device)
-
-
-
-# Training loop
-
-for epoch in range(num_epochs):
-    print(f"Epoch {epoch}")
-    iterator = iter(train_loader)
-    for batch_num, batch in enumerate(iterator):
-        # Forward pass
-        transformer.train()
-        scaler = GradScaler()
-        sentence, target = batch
-        
-        self_attention_mask = create_decoder_masks(sentence)
-        optimizer.zero_grad()
-        with autocast():
-            prediction = transformer(sentence, self_attention_mask.to(device), dec_end_token=True, dec_start_token=False)
-            labels = transformer.decoder.embedding.batch_tokenize(target, start_token=False, end_token=True)
-            loss = criterion(prediction.view(-1, prediction.size(-1)).to(device), (labels.view(-1)).to(device)).to(device)
-            valid_indicies = torch.where(labels.view(-1) == language_to_index[PADDING_TOKEN], False, True)
-            loss = loss.sum() / valid_indicies.sum()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-        
-        if batch_num % 2 == 0:
-            print(f"Epoch {epoch} Batch {batch_num} Loss {loss.item():.4f}") 
-            print(f"input: {sentence[0]}")
-            print(f"target: {target[0]}")
-            text_generated = torch.argmax(prediction[0], axis=1) # type: ignore
-            predicted_sentence = ""
-            for idx in text_generated:
-                if idx == language_to_index[END_TOKEN]:
-                    break
-                index = idx.item()
-                predicted_sentence += index_to_language[index]
-            
-            print(f"Text Generation: {predicted_sentence}")
-
-        del sentence, target, self_attention_mask, prediction, labels, loss
-        torch.cuda.empty_cache()
-
-
-torch.save(transformer.state_dict(), "Decoder_Only_Transformer_v1.pth")
-
