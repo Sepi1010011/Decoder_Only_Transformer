89276d7628cb434d26b024119f5c31ac8ecfa328
unknown <ssimkhah@yahoo.com>
2024-04-25 22:00:54 +0330

    Changing the embedding architecture and using Bert embedding and also Changing loss and text generation and the dimension and learning rate and vocabulary for training

--- decoder_only_transformer_trainer.py
+++ decoder_only_transformer_trainer.py
@@ -1,33 +1,40 @@
 import random
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import os
 from torch.utils.data import Dataset, DataLoader
 from torch.cuda.amp import autocast, GradScaler
 from decoder_only_transformer import create_decoder_masks, GPTLanguageModel, get_device
+from transformers import BertTokenizer, BertModel
 
 
-d_model = 128
+
+d_model = 768
 num_layers = 4
 num_heads = 2
 batch_size = 15
-learning_rate = 0.0006
+learning_rate = 0.00002
 block_number = 4
 drop_prob = 0.2
 NEG_INFTY = -1e9
 num_epochs = 10
 ffn_hidden = 2048
 
+PRETRAINED_MODEL = "bert-base-uncased"
+START_TOKEN = '[START]'
+END_TOKEN = '[END]'
+PADDING_TOKEN = '[PAD]'
+TOTAL_SENTENCES = 200000
+max_sequence_length = 512
 
-START_TOKEN = '<start>'
-END_TOKEN = '<end>'
-PADDING_TOKEN = '<pad>'
-TOTAL_SENTENCES = 100000
-max_sequence_length = 2048
-english_vocabulary = [START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',':', '<', '=', '>', '?', '@','[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', PADDING_TOKEN, END_TOKEN]
-en_vocab_size = len(english_vocabulary)
-language_to_index = {v:k for k,v in enumerate(english_vocabulary)}
-index_to_language = {k:v for k,v in enumerate(english_vocabulary)}
+tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL)
+bert_model = BertModel.from_pretrained(PRETRAINED_MODEL)
+
+english_vocabulary = tokenizer.get_vocab()
+token_to_index = {token: index for token, index in english_vocabulary.items()}
+index_to_token = {index: token for token, index in english_vocabulary.items()}
+en_vocab_size = 30522 
 
 file_input = open("input.txt", 'r')
 dataset_input = file_input.readlines()
@@ -69,25 +76,26 @@ class TextDataset(Dataset):
         return self.input[idx], self.target[idx]
 
 
-dataset = TextDataset(dataset_input, dataset_target)
+dataset = TextDataset(randomly_input, randomly_target )
 train_loader = DataLoader(dataset, batch_size)
 
 
-transformer = GPTLanguageModel(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, en_vocab_size, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+transformer = GPTLanguageModel(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, en_vocab_size)
 
 # Define loss function and optimizer
 for params in transformer.parameters():
     if params.dim() > 1:
         nn.init.xavier_uniform_(params)
         
-criterion = nn.CrossEntropyLoss(reduction='none')
+criterion = nn.CrossEntropyLoss(reduction='none', ignore_index=token_to_index[PADDING_TOKEN])
 optimizer = torch.optim.Adam(transformer.parameters(), lr=learning_rate)
 device = get_device()
+target_linear = nn.Linear(768, 1)
 
 transformer.train()
 transformer.to(device)
 
-checkpoint_interval = 2
+checkpoint_interval = 1
 checkpoint_dir = "Checkpoints"
 
 os.makedirs(checkpoint_dir, exist_ok=True)
@@ -108,33 +116,42 @@ for epoch in range(num_epochs):
         self_attention_mask = create_decoder_masks(sentence)
         optimizer.zero_grad()
         with autocast():
-            prediction = transformer(sentence, self_attention_mask.to(device), dec_end_token=True, dec_start_token=False)
-            labels = transformer.decoder.embedding.batch_tokenize(target, start_token=False, end_token=True)
-            loss = criterion(prediction.view(-1, prediction.size(-1)).to(device), (labels.view(-1)).to(device)).to(device)
-            valid_indicies = torch.where(labels.view(-1) == language_to_index[PADDING_TOKEN], False, True)
+            prediction = transformer(sentence, self_attention_mask.to(device))
+            input = tokenizer(target, padding="max_length", truncation=True, max_length=max_sequence_length, return_tensors="pt",)
+            outputs = bert_model(**input) 
+            target_embedded = outputs.last_hidden_state
+            target_embedded = target_linear(target_embedded).to(device)
+            
+            loss = criterion((prediction.view(-1, prediction.size(-1))).to(device), (target_embedded.view(-1).long()).to(device)).to(device)
+            valid_indicies = torch.where(target_embedded.view(-1) == token_to_index[PADDING_TOKEN], False, True)
             loss = loss.sum() / valid_indicies.sum()
+            
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
         
         total_loss += loss.item()
         
-        if batch_num % 25 == 0:
+        if batch_num % 2 == 0:
             print(f"Epoch {epoch} Batch {batch_num} Loss {loss.item():.4f}") 
             print(f"input: {sentence[0]}")
             print(f"target: {target[0]}")
-            text_generated = torch.argmax(prediction[0], axis=1) # type: ignore
-            predicted_sentence = ""
-            for idx in text_generated:
-                if idx == language_to_index[END_TOKEN]:
-                    break
-                index = idx.item()
-                predicted_sentence += index_to_language[index]
             
-            print(f"Text Generation: {predicted_sentence}")
-            
-        del sentence, target, self_attention_mask, prediction, labels, loss
-        torch.cuda.empty_cache()
+            bert_embeddings = (bert_model.get_input_embeddings().weight).to(device)
+            decoder_output_flat = prediction.view(-1, prediction.size(-1)).to(device)  
+
+            similarities = torch.matmul(decoder_output_flat, bert_embeddings).to(device)
+            similarities = similarities.view(1, max_sequence_length, -1).to(device)
+
+            softmax_scores = F.softmax(similarities, dim=-1).to(device)
+
+            predicted_token_ids = torch.argmax(softmax_scores, dim=-1).to(device)
+
+            predicted_tokens = [tokenizer.convert_ids_to_tokens(ids.tolist()) for ids in predicted_token_ids]
+
+            print(f"Text Generation: {predicted_tokens}")
+                                
+        # torch.cuda.empty_cache()
         
     if epoch % checkpoint_interval == 0:
         checkpoint_path = os.path.join(checkpoint_dir, f'checkpoint_epoch_{epoch}.pt')
@@ -144,7 +161,6 @@ for epoch in range(num_epochs):
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': total_loss
         }, checkpoint_path)
-
 
 
 torch.save(transformer.state_dict(), "Decoder_Only_Transformer_v1.pth")
